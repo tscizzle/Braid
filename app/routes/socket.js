9f1dcf86cc1fc759ecdce16e09202be52c184be4
Tyler <tscizzle@gmail.com>
2016-04-25 22:58:39 -0400

    socket only room is user room

--- app/routes/socket.js
+++ app/routes/socket.js
@@ -1,6 +1,15 @@
+var _ = require('underscore');
+
+
 module.exports = function(socket) {
 
     socket.on('room:join', function(user_data) {
+        // leave any other rooms
+        _.each(socket.rooms, function(room) {
+            socket.leave(room);
+        });
+
+        // then join this user room
         socket.join(user_data._id);
     });
 
