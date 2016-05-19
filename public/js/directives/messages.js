8f15fec6070641cb6b25e27c22cf6ea284d45b85
Tyler <tscizzle@gmail.com>
2016-05-18 21:34:47 -0400

    stylin

--- public/js/directives/messages.js
+++ public/js/directives/messages.js
@@ -254,7 +254,7 @@ angular.module('messagesDirective', [])
                 message_color = COLOR_TO_FADED_MAP[vm.thisColor()];
             // if a message is neither in a strand nor primed, make it no color
             } else {
-                message_color = 'white';
+                message_color = '#DDD';
             };
             return message_color;
         };
@@ -302,7 +302,7 @@ angular.module('messagesDirective', [])
                 textarea_color = COLOR_TO_FADED_MAP[vm.thisColor()];
             // if there is no selected strand and no primed messages, make it no color
             } else {
-                textarea_color = 'white';
+                textarea_color = '#EEE';
             };
             return textarea_color;
         };
