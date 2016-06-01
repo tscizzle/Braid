9c0156792b410ce515848277909a668f8da731dc
Tyler <tscizzle@gmail.com>
2016-06-01 01:17:46 -0400

    print mongo url

--- app/config/database.js
+++ app/config/database.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 
 var MONGO_CONNECTION_STRING = process.env.MONGOLAB_URI || "mongodb://localhost:27017"
+console.log('MONGO_CONNECTION_STRING', MONGO_CONNECTION_STRING);
 
 mongoose.connect(MONGO_CONNECTION_STRING);
