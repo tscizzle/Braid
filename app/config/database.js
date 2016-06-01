e25454c1433063184ea1ec6bf1c5d3af5a41796e
Tyler <tscizzle@gmail.com>
2016-06-01 01:19:05 -0400

    took it out

--- app/config/database.js
+++ app/config/database.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 
-var MONGO_CONNECTION_STRING = process.env.MONGOLAB_URI || "mongodb://localhost:27017"
-console.log('MONGO_CONNECTION_STRING', MONGO_CONNECTION_STRING);
+var MONGO_CONNECTION_STRING = process.env.MONGOLAB_URI || "mongodb://localhost:27017";
 
 mongoose.connect(MONGO_CONNECTION_STRING);
