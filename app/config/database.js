12b39cb3255c47c5011156ce368a39b4d04a101b
Tyler <tscizzle@gmail.com>
2016-07-25 23:24:15 -0400

    mongolab on heroku changed their default mongo db url env variable name to mongodb_uri

--- app/config/database.js
+++ app/config/database.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
-var MONGO_CONNECTION_STRING = process.env.MONGOLAB_URI || "mongodb://localhost:27017";
+var MONGO_CONNECTION_STRING = process.env.MONGODB_URI || "mongodb://localhost:27017";
 
 mongoose.connect(MONGO_CONNECTION_STRING);
