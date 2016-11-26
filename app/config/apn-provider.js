dd896e74e566712b0682b936aa1738f1d2aa28af
Tyler <tscizzle@gmail.com>
2016-11-26 17:10:49 -0500

    put cert and key into env variables instead of static files

--- app/config/apn-provider.js
+++ app/config/apn-provider.js
@@ -1,8 +1,8 @@
 var apn = require('apn');
 
 var apnProvider = new apn.Provider({
-  cert: 'ignore/aps_development.pem', // TODO: make these strings of these files' contents? somehow make multi-line env vars?
-  key: 'ignore/aps_key_development.pem' // TODO: make these strings of these files' contents? somehow make multi-line env vars?
+  cert: process.env.APS_CERT,
+  key: process.env.APS_KEY
 });
 
 module.exports = apnProvider;
