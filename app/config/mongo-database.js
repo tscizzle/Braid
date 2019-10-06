59d29b69a167192ed9d8b70ad3e67b3be93160b0
Tyler S-C <tscizzle@gmail.com>
2019-10-06 18:28:47 -0400

    updated bower, and fixed a couple warnings due to outdated code (man this code is old)

--- app/config/mongo-database.js
+++ app/config/mongo-database.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
 
-var MONGO_CONNECTION_STRING = process.env.MONGODB_URI || "mongodb://localhost:27017";
+var MONGO_CONNECTION_STRING =
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
 
 mongoose.connect(MONGO_CONNECTION_STRING);
--- app/routes/pages.js
+++ app/routes/pages.js
@@ -1,22 +1,30 @@
-module.exports = function(app) {
+const path = require('path');
 
+module.exports = function(app) {
     // For known routes, simply send back the main page index.html.
     // The frontend does the actual rendering of different views.
 
     app.get('/', function(req, res) {
-        return res.sendfile('./public/index/index.html');
+        return res.sendFile(
+            path.resolve(__dirname + '/../../public/index/index.html')
+        );
     });
 
     app.get('/auth', function(req, res) {
-        return res.sendfile('./public/index/index.html');
+        return res.sendFile(
+            path.resolve(__dirname + '/../../public/index/index.html')
+        );
     });
 
     app.get('/profile', function(req, res) {
-        return res.sendfile('./public/index/index.html');
+        return res.sendFile(
+            path.resolve(__dirname + '/../../public/index/index.html')
+        );
     });
 
     app.get('/help', function(req, res) {
-        return res.sendfile('./public/index/index.html');
+        return res.sendFile(
+            path.resolve(__dirname + '/../../public/index/index.html')
+        );
     });
-
 };
