b41cf820f65dd6ad93bbf34a66c06583fc5a6249
Tyler S-C <tscizzle@gmail.com>
2019-10-06 18:39:24 -0400

    npm audit fix stuff

--- app/config/mongo-database.js
+++ app/config/mongo-database.js
@@ -3,4 +3,8 @@ var mongoose = require('mongoose');
 var MONGO_CONNECTION_STRING =
   process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
 
-mongoose.connect(MONGO_CONNECTION_STRING);
+mongoose.connect(MONGO_CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
