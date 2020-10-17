8593f150f54bcb9e52afe91d07ff981c03b5fe59
Tyler S-C <tscizzle@gmail.com>
2020-10-17 14:11:00 -0400

    Changed mongo db config var name, as part of migration from mlab to atlas

--- app/config/mongo-database.js
+++ app/config/mongo-database.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var MONGO_CONNECTION_STRING =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/test';
+  process.env.ATLAS_MONGODB_URI || 'mongodb://localhost:27017/test';
 
 mongoose.connect(MONGO_CONNECTION_STRING, {
   useNewUrlParser: true,
